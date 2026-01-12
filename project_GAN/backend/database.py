ad5ca9ce405e1319d6d18e9614e0a7f0a984c1e6
Elizaveta Malezh <milkasps66@gmail.com>
2026-01-12 12:44:37 +0300

    V3.0
    Настройка подключения к новому облачному хранилищу для хранения изображений с использованием AWS S3

--- project_GAN/backend/database.py
+++ project_GAN/backend/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 from config import Config
+import boto3
+
+s3 = boto3.client('s3')
+s3.upload_file(file_path, 'bucket-name', 'image-key')
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 Base.metadata.create_all(engine)
